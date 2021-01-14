612263a75fe238f7bbd9c91d86d74e1c222ce92a
Eetu Niemi <eetu.niemi@vincit.fi>
2021-01-14 14:38:06 +0200

    Disable unused and insecure routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -9,14 +9,15 @@ Route::group([
     'prefix'     => $chatRoutesPrefix,
 ], function () {
     /* Conversation */
-    Route::get('/conversations', 'ConversationController@index')->name('conversations.index');
+    /*     Route::get('/conversations', 'ConversationController@index')->name('conversations.index');
+ */
     Route::post('/conversations', 'ConversationController@store')->name('conversations.store');
-    Route::get('/conversations/{id}', 'ConversationController@show')->name('conversations.show');
+    /* Route::get('/conversations/{id}', 'ConversationController@show')->name('conversations.show');
     Route::put('/conversations/{id}', 'ConversationController@update')->name('conversations.update');
-    Route::delete('/conversations/{id}', 'ConversationController@destroy')->name('conversations.destroy');
+    Route::delete('/conversations/{id}', 'ConversationController@destroy')->name('conversations.destroy'); */
 
     /* Conversation Participation */
-    Route::post('/conversations/{id}/participants', 'ConversationParticipationController@store')
+    /* Route::post('/conversations/{id}/participants', 'ConversationParticipationController@store')
         ->name('conversations.participation.store');
     Route::delete('/conversations/{id}/participants/{participation_id}', 'ConversationParticipationController@destroy')
         ->name('conversations.participation.destroy');
@@ -25,17 +26,17 @@ Route::group([
     Route::put('/conversations/{id}/participants/{participation_id}', 'ConversationParticipationController@update')
         ->name('conversations.participation.update');
     Route::get('/conversations/{id}/participants', 'ConversationParticipationController@index')
-        ->name('conversations.participation.index');
+        ->name('conversations.participation.index'); */
 
     /* Messaging */
     Route::post('/conversations/{id}/messages', 'ConversationMessageController@store')
         ->name('conversations.messages.store');
     Route::get('/conversations/{id}/messages', 'ConversationMessageController@index')
         ->name('conversations.messages.index');
-    Route::get('/conversations/{id}/messages/{message_id}', 'ConversationMessageController@show')
+    /* Route::get('/conversations/{id}/messages/{message_id}', 'ConversationMessageController@show')
         ->name('conversations.messages.show');
     Route::delete('/conversations/{id}/messages', 'ConversationMessageController@deleteAll')
         ->name('conversations.messages.destroy.all');
     Route::delete('/conversations/{id}/messages/{message_id}', 'ConversationMessageController@destroy')
-        ->name('conversations.messages.destroy');
+        ->name('conversations.messages.destroy'); */
 });
